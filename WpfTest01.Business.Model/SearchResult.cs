ab8a5de751a6518bf4daea396b3627dd8c2b2805
Robert Taylor <rtaylor@global-aero.com>
2020-05-28 10:12:54 +0100

    Added quoted premium to search results

--- WpfTest01.Business.Model/SearchResult.cs
+++ WpfTest01.Business.Model/SearchResult.cs
@@ -7,5 +7,6 @@ namespace WpfTest01.Business.Model
         public string InsuredName { get; set; }
         public DateTime InceptionDate { get; set; }
         public bool IsBound { get; set; }
+        public decimal QuotedPremium { get; set; }
     }
 }
--- WpfTest01.Business.Service/SearchService.cs
+++ WpfTest01.Business.Service/SearchService.cs
@@ -17,18 +17,21 @@ namespace WpfTest01.Business.Service
             var randomGenerator = new Random();
             var randomDateRange = (DateTime.Today - _startDate).Days;  
 
+            //Generate some random results
             for (var i = 0; i < 100; i++)
             {
                 var result = new SearchResult
                 {
                     InsuredName = $"{insuredName.ToLower()}-{Path.GetRandomFileName().Replace(".", string.Empty)}", 
                     InceptionDate = _startDate.AddDays(randomGenerator.Next(randomDateRange)), 
-                    IsBound = randomGenerator.Next(2) == 0
+                    IsBound = randomGenerator.Next(2) == 0,
+                    QuotedPremium = Math.Round((decimal) randomGenerator.NextDouble() * 100000M)
                 };
 
                 results.Add(result);
             }
 
+            //Sleep to mimic a slow service call
             Thread.Sleep(TimeSpan.FromSeconds(3));
 
             return results;
